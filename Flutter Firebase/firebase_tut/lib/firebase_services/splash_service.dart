a9acd2057e8efbfa5ea3ca852c833a122f8f3e5d
Muhammad Bilal <muhammadbilalmnaeem@gmail.com>
2023-02-16 21:51:34 +0500

    firebase logout and check login

--- Flutter Firebase/firebase_tut/lib/firebase_services/splash_service.dart
+++ Flutter Firebase/firebase_tut/lib/firebase_services/splash_service.dart
@@ -1,17 +1,40 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_tut/ui/auth/login_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../ui/post_screen/post_screen.dart';
+
 class SplashService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   void isLogin(BuildContext context) {
-    Timer(
-      const Duration(seconds: 3),
-      () {
-        Navigator.pushReplacement(
-          context,MaterialPageRoute(builder: (context) => const LoginScreen(),),
-        );
-      },
-    );
+    final user = _auth.currentUser;
+
+    if (user != null) {
+      Timer(
+        const Duration(seconds: 3),
+        () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const PostScreen(),
+            ),
+          );
+        },
+      );
+    } else {
+      Timer(
+        const Duration(seconds: 3),
+        () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const LoginScreen(),
+            ),
+          );
+        },
+      );
+    }
   }
 }
--- Flutter Firebase/firebase_tut/lib/ui/post_screen/post_screen.dart
+++ Flutter Firebase/firebase_tut/lib/ui/post_screen/post_screen.dart
@@ -1,14 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_tut/ui/auth/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class PostScreen extends StatelessWidget {
+class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
 
   @override
+  State<PostScreen> createState() => _PostScreenState();
+}
+
+class _PostScreenState extends State<PostScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Post"),),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text("Post"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                _auth.signOut();
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginScreen(),
+                    ));
+              },
+              icon: const Icon(Icons.logout_outlined))
+        ],
+      ),
     );
   }
 }
